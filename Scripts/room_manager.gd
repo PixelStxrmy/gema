e6904961ab4ca1ed91a9179067b56a2e9edcf0ed
Stxrmy (AKA William) <302270170@student.rocmondriaan.nl>
2025-04-10 18:36:42 +0200

    sutff imports??
    i forgor, I'm just making sure all my files are up to date on my laptop :3

--- Scripts/room_manager.gd
+++ Scripts/room_manager.gd
@@ -9,15 +9,24 @@ var first_room = 1
 
 func _ready() -> void:
 	add_room(first_room)
+	del_room(get_current_room())
 
 
-func get_room(num:String):
+func get_new_room(num:String):
 	var room = load(room_path0 + num + room_path1).instantiate()
 	if (room):
 		print(room)
 		return room
 
+func get_current_room():
+	var room = self.get_child(0)
+	return room
+
 func add_room(room):
-	var loaded_room = get_room(str(room))
+	var loaded_room = get_new_room(str(room))
 	print(loaded_room)
 	self.add_child(loaded_room)
+
+func del_room(room):
+	self.remove_child(room)
+	print("deleted!")
